cd3b143adb683381b97ea93e1efef9df099db1b0
mmainero <maineromatias@gmail.com>
2024-09-05 21:45:00 -0300

    tabla country

--- resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -3,8 +3,34 @@
 @includeWhen(class_exists(\Backpack\DevTools\DevToolsServiceProvider::class), 'backpack.devtools::buttons.sidebar_item')
 
 
-<x-backpack::menu-item title="Users" icon="la la-users" :link="backpack_url('user')" />
-<!-- <x-backpack::menu-item title="Countries" icon="la la-question" :link="backpack_url('country')" />
-<x-backpack::menu-item title="Funtions" icon="la la-question" :link="backpack_url('funtion')" />
-<x-backpack::menu-item title="Themes" icon="la la-question" :link="backpack_url('theme')" />
-<x-backpack::menu-item title="Series" icon="la la-question" :link="backpack_url('serie')" /> -->
+<!--<x-backpack::menu-item title="Users" icon="la la-users" :link="backpack_url('user')" />
+ <x-backpack::menu-item title="Countries" icon="la la-globe" :link="backpack_url('country')" />
+<x-backpack::menu-item title="Funtions" icon="la la-cogs" :link="backpack_url('funtion')" />
+<x-backpack::menu-item title="Themes" icon="la la-paint-brush" :link="backpack_url('theme')" />
+<x-backpack::menu-item title="Series" icon="la la-film" :link="backpack_url('serie')" /> -->
+
+<!-- 
+ -->
+<!-- Parametricas Dropdown -->
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="parametricasDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <i class="la la-cogs nav-icon"></i> Parametricas
+    </a>
+    <ul class="dropdown-menu" aria-labelledby="parametricasDropdown">
+        <li>
+            <x-backpack::menu-item title="Users" icon="la la-users" :link="backpack_url('user')" />
+        </li>
+        <li>
+            <x-backpack::menu-item title="Countries" icon="la la-globe" :link="backpack_url('country')" />
+        </li>
+        <!--<li>
+            <x-backpack::menu-item title="Functions" icon="la la-cogs" :link="backpack_url('function')" />
+        </li>
+        <li>
+            <x-backpack::menu-item title="Themes" icon="la la-paint-brush" :link="backpack_url('theme')" />
+        </li>
+        <li>
+            <x-backpack::menu-item title="Series" icon="la la-film" :link="backpack_url('serie')" />
+        </li>-->
+    </ul>
+</li>
